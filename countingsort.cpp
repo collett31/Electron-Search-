9f0dc7f7612446d33a06deaabe7820ef858a4aa7
collett <charltoncollett1@gmail.com>
2019-09-29 21:25:57 -0500

    .

---
+++ countingsort.cpp
@@ -0,0 +1,47 @@
+ //Counting sort which takes negative numbers as well 
+#include <iostream> 
+#include <vector> 
+#include <algorithm> 
+using namespace std; 
+
+void countSort(vector <int>& arr) 
+{ 
+	int max = *max_element(arr.begin(), arr.end()); 
+	int min = *min_element(arr.begin(), arr.end()); 
+	int range = max - min + 1; 
+	vector<int> count(range), output(arr.size());
+
+	for(int i = 0; i < arr.size(); i++) 
+		count[arr[i]-min]++; 
+		
+	for(int i = 1; i < count.size(); i++) 
+		count[i] += count[i-1]; 
+	
+	for(int i = arr.size()-1; i >= 0; i--) 
+	{ 
+		output[ count[arr[i]-min] -1 ] = arr[i]; 
+		count[arr[i]-min]--; 
+	} 
+	
+	for(int i=0; i < arr.size(); i++) 
+        arr[i] = output[i]; 
+} 
+
+void printArray(vector <int> & arr) 
+{ 
+	for (int i=0; i < arr.size(); i++) 
+		cout << arr[i] << " "; 
+	cout << "\n"; 
+} 
+
+int main()  
+{  
+    vector <int> arr;
+    for (int i = 0; i < 1000000; i++)
+    {
+        arr.push_back(rand());
+    }
+    countSort(arr);
+    //countSort(arr); 
+    return 0;  
+}  
